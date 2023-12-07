e64c6e97f1c44b9f05fc482f0884cce2df65d88f
Adam McIntire <757770@student.dusd.net>
2023-12-07 15:24:28 -0800

    Added bird and on script video
    Added bird and on script video

---
+++ FlappyBirdP8McIntire/Assets/Scripts/Bird.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upforce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+            }
+        }
+    }
+}
