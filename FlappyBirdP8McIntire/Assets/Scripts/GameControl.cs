a077efaaed5b6e17c5be4243cb07bdb6aea60c2d
Adam McIntire <757770@student.dusd.net>
2023-12-13 15:28:12 -0800

    Scene is scrolling bird is animating
    Scene is scrolling bird is animating

--- FlappyBirdP8McIntire/Assets/Scripts/GameControl.cs
+++ FlappyBirdP8McIntire/Assets/Scripts/GameControl.cs
@@ -6,18 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
-    public static GameControl Instance;
+    public static GameControl instance;
     public GameObject gameOverText;
     public TextMeshProUGUI ScoreText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
     }
 }
